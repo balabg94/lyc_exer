98791c2f21f21768940c6f1eba30cfde4c1983cd
balabg94 <balagovind2094@gmail.com>
2018-08-20 14:04:50 +0530

    Wrote a crude script to convert a dictionary to json format and write it into a json file. Will be working with JSON as the map container.

---
+++ Interactive_fiction/dict2json.py
@@ -0,0 +1,20 @@
+import json
+
+dictio = {'0': ['You are standing at the end of a corridor. There is a door in front of you.', '0','1', '0', '0'],
+          '1': ['You are standing in what seems to be a living room.', '1', '0', '1', '0']
+}
+dumped_dictio = json.dumps(dictio)
+loaded_dictio = json.loads(dumped_dictio)
+
+print (loaded_dictio['0'][0])
+
+# json_file = open("maps.json", "w")
+
+# json_file.write(loaded_dictio)
+
+# json_file.close()
+
+with open('maps.json', 'w') as outfile:
+    json.dump(loaded_dictio, outfile)
+
+outfile.close()
