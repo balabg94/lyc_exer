31313faad9be46a8661a7a95c738d9585ad6f77e
Balagovind <balagovind2094@gmail.com>
2018-08-23 10:41:12 +0530

    RED: Zeroth room load failed.

--- Interactive_fiction/test_ifs.py
+++ Interactive_fiction/test_ifs.py
@@ -4,3 +4,8 @@ import engine
 def test_map():
     #to test if a json file is loaded.
     assert engine.open_map("./maps.json") == True
+
+def test_if_firstroom():
+    #to test if the first room is loaded.
+
+    assert engine.zeroth_room() == "You are standing at the end of a corridor. There is a door in front of you."
