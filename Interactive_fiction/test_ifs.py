77c3e5d5cff251780f32f419c899f4c2091ed251
Balagovind <balagovind2094@gmail.com>
2018-08-24 14:26:15 +0530

    RED: Incase of wrong exits

--- Interactive_fiction/test_ifs.py
+++ Interactive_fiction/test_ifs.py
@@ -14,3 +14,7 @@ def test_get_exits():
     #to test if exists from current room are available
     current_room = "0"
     assert engine.get_exits(current_room) == [['S', 1]]
+
+def test_no_exit():
+    # to test the output in case of no exit
+    assert engine.if_exit("N") == "No exit that way"
